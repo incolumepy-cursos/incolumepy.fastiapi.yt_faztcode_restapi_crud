51098f80cce329a86fc0d54d5f89913fdb50b8f3
Britodfbr <britodfbr@gmail.com>
2022-07-12 11:08:55 -0300

    refactor: Atualizado recurso fstring
    definido Python 3.8+

--- incolumepy/fastapi/yt_faztcode_restapi_crud/__init__.py
+++ incolumepy/fastapi/yt_faztcode_restapi_crud/__init__.py
@@ -10,12 +10,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 pyproject = Path(__file__).parents[3] / 'pyproject.toml'
 # assert pyproject.is_file(), f'{pyproject}'
-logging.debug(f'{pyproject}')
+logging.debug(f'{pyproject=}')
 
 version_file = Path(__file__).parent / 'version.txt'
-logging.debug(f'{version_file}')
+logging.debug(f'{version_file=}')
 
 with pyproject.open('rb') as f:
     version_file.write_text(tomllib.load(f)['tool']['poetry']['version'])
 __version__ = version_file.read_text()
-logging.info(f'{__version__}')
+logging.info(f'{__version__=}')
